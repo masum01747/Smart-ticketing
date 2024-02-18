ed05fec0a1aab5cdba125901997cffc948f6656b
masum01747 <mohammad7masum@gmail.com>
2024-02-19 01:37:58 +0600

    utility js added

---
+++ script.js
@@ -0,0 +1,28 @@
+const aOne = document.getElementById('aOne');
+aOne.addEventListener = ('click', function(){
+    aOne.style.backgroundColor='Red'
+})
+function greenColor(){
+    document.getElementById('bTwo').style.backgroundColor = 'Green'
+let totalSeat = 40
+ let remainningSeat = document.getElementById('totalSeat').innerText = totalSeat-1
+//    totalSeat -= totalSeat
+let seat=0
+ let addingSeat = document.getElementById('seat').innerText = seat+1
+// seat.innerText ='1'
+
+ 
+
+}
+// function greenColor(){
+//     document.getElementById('bOne').style.backgroundColor = 'Green'
+// let totalSeat = 40
+//  let remainningSeat = document.getElementById('totalSeat').innerText = totalSeat-1
+// //    totalSeat -= totalSeat
+// let seat=0
+//  let addingSeat = document.getElementById('seat').innerText = seat+1
+// // seat.innerText ='1'
+
+ 
+
+// }
