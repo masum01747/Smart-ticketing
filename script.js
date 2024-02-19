1b8371950c2fffb38eca27cbd20e35eca8358c8a
masum01747 <mohammad7masum@gmail.com>
2024-02-19 11:27:14 +0600

    changed color

--- script.js
+++ script.js
@@ -60,9 +60,31 @@ for(const btn of allBtn){
     btn.addEventListener('click',function(e){
         count=count+1
         const seatNumber = e.target.innerText
+        const targetPlace = document.getElementById('selectedPlace');
+        const li = document.createElement('li');
+        const p = document.createElement('P');
+        p.innerText = seatNumber
+        const p2 = document.createElement('P');
+        p2.innerText = 'Economy'
+        const p3 = document.createElement('P');
+        p3.innerText = parseInt('550');
+        li.appendChild(p);
+        li.appendChild(p2);
+        li.appendChild(p3);
+        targetPlace.appendChild(li);
+        const totalCost = document.getElementById('totalCost').innerText ;
+        const convertedTotalcost = parseInt(totalCost);
+        const convertedinitisl = parseInt(550)
+        const sum = convertedTotalcost +parseInt('550')
+        //  document.getElementById('totalCost').innerText = sum;
+        const grandTotal = document.getElementById('grandValu').innerText;
+        const grandConverted = parseInt(grandTotal);
+        const sum2 = grandConverted+parseInt('550');
+        document.getElementById('grandValu').innerText = sum2;
 
 
 
+        setInnerTex('totalCost',sum)
         setInnerTex('seat',count)
     })
 }
