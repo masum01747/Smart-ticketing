461af998122ab4aa65685695569cf3fbd27ee917
masum01747 <mohammad7masum@gmail.com>
2024-02-19 10:35:04 +0600

    Finish Total

--- script.js
+++ script.js
@@ -7,38 +7,66 @@
 function play(){
     hideElementById('pBahan');
     showElementById('ticketSeat');
+//     addBackGroundColorById('aOne')
+//     addBackGroundColorById('aTwo')
+//     addBackGroundColorById('aThree')
+//     addBackGroundColorById('aFour')
 }
 
+    // addBackGroundColorById('aOne')
+    // addBackGroundColorById('aTwo')
+    // addBackGroundColorById('aThree')
+    // addBackGroundColorById('aFour')
 
 
 
 
 
-const aOne = document.getElementById('aOne');
-aOne.addEventListener = ('click', function(){
-    aOne.style.backgroundColor='Red'
-})
-function greenColor(){
-    document.getElementById('bTwo').style.backgroundColor = 'Green'
-let totalSeat = 40
- let remainningSeat = document.getElementById('totalSeat').innerText = totalSeat-1
-//    totalSeat -= totalSeat
-let seat=0
- let addingSeat = document.getElementById('seat').innerText = seat+1
-// seat.innerText ='1'
-
- 
-
-}
+// const aOne = document.getElementById('aOne');
+// aOne.addEventListener = ('click', function(){
+//     aOne.style.backgroundColor='Red'
+// })
 // function greenColor(){
-//     document.getElementById('bOne').style.backgroundColor = 'Green'
+//     document.getElementById('bTwo').style.backgroundColor = 'Green'
 // let totalSeat = 40
 //  let remainningSeat = document.getElementById('totalSeat').innerText = totalSeat-1
 // //    totalSeat -= totalSeat
-// let seat=0
-//  let addingSeat = document.getElementById('seat').innerText = seat+1
+
+//  const currentSeat  = document.getElementById('seat');
+//  const currentSeatText = currentSeat.innerText;
+//  const currentSeatelement =parseInt(currentSeatText);
+//  const newSeat = currentSeatelement+1;
+//  currentSeat.innerText = newSeat
 // // seat.innerText ='1'
 
  
 
 // }
+// // function greenColor(){
+// //     document.getElementById('bOne').style.backgroundColor = 'Green'
+// // let totalSeat = 40
+// //  let remainningSeat = document.getElementById('totalSeat').innerText = totalSeat-1
+// // //    totalSeat -= totalSeat
+// // let seat=0
+// //  let addingSeat = document.getElementById('seat').innerText = seat+1
+// // // seat.innerText ='1'
+
+ 
+
+// // }
+let count = 0;
+const allBtn = document.getElementsByClassName('add-btn');
+for(const btn of allBtn){
+    btn.addEventListener('click',function(e){
+        count=count+1
+        const seatNumber = e.target.innerText
+
+
+
+        setInnerTex('seat',count)
+    })
+}
+
+function setInnerTex(id,value){
+    document.getElementById(id).innerText = value; 
+}
--- utility.js
+++ utility.js
@@ -8,3 +8,20 @@ function showElementById(elementId){
 
     
 }
+function addBackGroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-green-800');
+    // adding seat
+    const currentSeat  = document.getElementById('seat');
+ const currentSeatText = currentSeat.innerText;
+ const currentSeatelement =parseInt(currentSeatText);
+ const newSeat = currentSeatelement+1;
+ currentSeat.innerText = newSeat
+//  seat adjustment
+const currentSeatcapacity = document.getElementById('totalSeat');
+const currentseattext = currentSeatcapacity.innerText;
+const currentTotalSeat = parseInt(currentseattext);
+const newTotalSrat = currentTotalSeat -1;
+currentSeatcapacity.innerText = newTotalSrat
+
+}
