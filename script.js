a817ac28bef8f5d5ad19e52285ce3be1ebfc641e
masum01747 <mohammad7masum@gmail.com>
2024-02-19 22:33:32 +0600

    added coupn

--- script.js
+++ script.js
@@ -1,59 +1,10 @@
-// function play(){
-//     const pBahan= document.getElementById('pBahan');
-//     pBahan.classList.add('hidden');
-//     const ticket =document.getElementById('ticketSeat');
-//     ticket.classList.remove('hidden')
-// }
+
 function play(){
     hideElementById('pBahan');
     showElementById('ticketSeat');
-//     addBackGroundColorById('aOne')
-//     addBackGroundColorById('aTwo')
-//     addBackGroundColorById('aThree')
-//     addBackGroundColorById('aFour')
 }
 
-    // addBackGroundColorById('aOne')
-    // addBackGroundColorById('aTwo')
-    // addBackGroundColorById('aThree')
-    // addBackGroundColorById('aFour')
 
-
-
-
-
-// const aOne = document.getElementById('aOne');
-// aOne.addEventListener = ('click', function(){
-//     aOne.style.backgroundColor='Red'
-// })
-// function greenColor(){
-//     document.getElementById('bTwo').style.backgroundColor = 'Green'
-// let totalSeat = 40
-//  let remainningSeat = document.getElementById('totalSeat').innerText = totalSeat-1
-// //    totalSeat -= totalSeat
-
-//  const currentSeat  = document.getElementById('seat');
-//  const currentSeatText = currentSeat.innerText;
-//  const currentSeatelement =parseInt(currentSeatText);
-//  const newSeat = currentSeatelement+1;
-//  currentSeat.innerText = newSeat
-// // seat.innerText ='1'
-
- 
-
-// }
-// // function greenColor(){
-// //     document.getElementById('bOne').style.backgroundColor = 'Green'
-// // let totalSeat = 40
-// //  let remainningSeat = document.getElementById('totalSeat').innerText = totalSeat-1
-// // //    totalSeat -= totalSeat
-// // let seat=0
-// //  let addingSeat = document.getElementById('seat').innerText = seat+1
-// // // seat.innerText ='1'
-
- 
-
-// // }
 let count = 0;
 let totalSeat = 40;
 
